1378baff33d5faeff01e27abf49ba47790ae1667
Andranique Boone <andranique.boone@gmail.com>
2019-03-19 01:14:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,11 +15,11 @@ def swap_elements(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
-  
-end 
+
+end
 
 def reverse_array(array)
-  array.reverse 
+  array.reverse
 end
 
 def kesha_maker(array)

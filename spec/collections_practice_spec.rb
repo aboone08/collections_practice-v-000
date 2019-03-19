e1f0102a30be3661c9794dd7e06fec5b9d41df85
Andranique Boone <andranique.boone@gmail.com>
2019-03-19 01:45:43 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,8 +30,8 @@ describe 'collections practice' do
     end
   end
 
-  # Question 4 Advanced 
-  describe '#swap_elements_from_to' do 
+  # Question 4 Advanced
+  describe '#swap_elements_from_to' do
     it 'specify the index of the element you would like to move to a new index' do
       expect(swap_elements_from_to(["a", "b", "c"], 0,2)).to eq(["c", "b", "a"])
     end

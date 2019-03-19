2ae0b259593bbc50aee90d18c9f0d0f2a9e22a1f
Andranique Boone <andranique.boone@gmail.com>
2019-03-19 02:03:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,7 +15,7 @@ def swap_elements(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
-  array.sort {|index,destination_index| index <=> destination_index }
+  array.sort {|index,destination_index| index[0] <=> destination_index[2] }
 end
 
 def reverse_array(array)

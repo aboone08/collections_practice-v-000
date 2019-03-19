f895872bcd83d92f6024b63c77cced0f68c30e6e
Andranique Boone <andranique.boone@gmail.com>
2019-03-19 01:45:45 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -35,7 +35,7 @@ describe 'collections practice' do
     it 'specify the index of the element you would like to move to a new index' do
       expect(swap_elements_from_to(["a", "b", "c"], 0,2)).to eq(["c", "b", "a"])
     end
-    end
+  end
 
   # Question 5
   describe '#reverse_array' do

cd2036d28950af50feb05fa060b1cc55e746d811
Andranique Boone <andranique.boone@gmail.com>
2019-03-19 00:40:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(array)
+  array.sort {|a,b| a <=> b}
+end
 
+def sort_array_desc(array)
+  array.sort {|b,a| a <=> b}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array.sort {|a,b| a[1] <=> b[2]}
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+
+end
+
+def find_a()
+
+end
+
+def sum_array()
+
+end
+
+def add_s()
+
+end
